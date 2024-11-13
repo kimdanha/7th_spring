c2a3bdf07274330c6fd8d104136dbd64cf2ed7b7
kimdanha <lily034686@gmail.com>
2024-11-13 21:18:07 +0900

    회원가입 api

---
+++ week08/Study/src/main/java/umc/study/domain/Review.java
@@ -0,0 +1,34 @@
+package umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String body;
+
+    private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL)
+    private final List<ReviewImage> reviewImageList = new ArrayList<>();
+}
